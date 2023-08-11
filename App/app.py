11f1dbd81187d7e40e6265d50467e846a877dd78
voy <wojciech.przybylski1@hotmail.com>
2023-08-11 19:58:28 +0100

    cleaned up and refactored app.py

--- App/app.py
+++ App/app.py
@@ -1,28 +1,3 @@
-
-
-# @app.route('/add')
-# def add():
-#     new_game = Games(name="New Game"
-#     db.session.add(new_game)
-#     db.session.commit()
-#     return "Added new game to database"
-
-# @app.route('/read')
-# def read():
-#     all_games = Games.query.all()
-#     games_string = ""
-#     for game in all_games:
-#         games_string += "<br>"+ game.name
-#     return games_string
-
-# @app.route('/update/<name>')
-# def update(name):
-#     first_game = Games.query.first()
-#     first_game.name = name
-#     db.session.commit()
-    # return first_game.name
-
-
 from application import create_app
 
 app = create_app()
